16cd63dea65541109dacb9a344ea5123c6b78ec3
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-22 14:56:21 +0000

    initial commit

---
+++ my/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { SearchBar1 } from './components/SearchBar1';
+
+function App() {
+  return (
+    <div className="App">
+     <SearchBar1 />
+    </div>
+  );
+}
+
+export default App;
