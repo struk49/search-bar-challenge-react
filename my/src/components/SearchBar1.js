7336292256ab773335080eb6fdd55d8669c34492
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-22 17:55:34 +0000

    completed searchbar challenge

--- my/src/components/SearchBar1.js
+++ my/src/components/SearchBar1.js
@@ -1,27 +1,41 @@
-
 import React, { Component } from 'react'
+import importedMNames from '../names'
 
 export class SearchBar1 extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
+            names: importedMNames
              
         }
     }
-    
+    handleChange = (event) => {
+        const inputText = event.target.value.toLowerCase()
+        console.log(inputText)
+        const filteredNames = importedMNames.filter(name => {
+            return name.toLowerCase().includes(inputText)
+        })
+        this.setState({
+            names: filteredNames
+        })
+    }
     render() {
         return (
             <div>
                 <h1>Name Search</h1>
+                <p>Matching names found...{this.state.names.length}</p>
                 <form>
                     <input
+                        onChange={(event) => this.handleChange(event)}
                         type="text"
                         placeholder="search names..."
                     />
                 </form>
                 <div style={{margin: 'auto'}}>
-                    we will render names here
+                    {this.state.names.map(name => {
+                        return <p key={name}>{name}</p>
+                    })}
                 </div>
             </div>
         )
